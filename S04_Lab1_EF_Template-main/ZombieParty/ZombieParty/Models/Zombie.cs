fa0a8825e1dd53d2f1b624d13fb23e50cc1994f3
Mohamed Qassine <2240905@cegepmontpetit.ca>
2025-09-12 14:16:36 -0400

    AJout des regles de specification
    ajout des required

--- S04_Lab1_EF_Template-main/ZombieParty/ZombieParty/Models/Zombie.cs
+++ S04_Lab1_EF_Template-main/ZombieParty/ZombieParty/Models/Zombie.cs
@@ -6,6 +6,7 @@ namespace ZombieParty.Models
     public class Zombie
     {
         public int Id { get; set; }
+        [Required, StringLength(20, MinimumLength =5)]
         public string Name { get; set; }
         // FACULTATIF on peut formellement identifier le champ lien
         // sinon le champ de foreignKey sera auto généré dans la BD
@@ -13,6 +14,10 @@ namespace ZombieParty.Models
         [ForeignKey("ZombieType")]
         public int ZombieTypeId { get; set; }
         public ZombieType ZombieType { get; set; }
+        [Range(1, 20)]
         public int Point { get; set; }
+        [StringLength(255)]
+        public string ShortDesc { get; set; }
+       
     }
 }
--- S04_Lab1_EF_Template-main/ZombieParty/ZombieParty/Models/ZombieType.cs
+++ S04_Lab1_EF_Template-main/ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -8,7 +8,9 @@ namespace ZombieParty.Models
         [Key]
         public int Id { get; set; }
         [DisplayName("Type Name")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} has to be filled.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} has to be filled."),StringLength(10, MinimumLength =5)]
         public string TypeName { get; set; }
+        [Range(2, 5)]
+        public int Point {  get; set; }
     }
 }
